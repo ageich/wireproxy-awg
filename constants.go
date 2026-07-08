ce8b7fc051c0b13cd36d2ab8466de80f5f63bb6b
ageich <53635625+ageich@users.noreply.github.com>
2026-07-09 00:40:56 +0700

    Update constants.go

--- constants.go
+++ constants.go
@@ -3,19 +3,20 @@ package wireproxy
 import "time"
 
 // Глобальные таймауты для всех TCP-соединений
+// Увеличены для повышения стабильности при медленных или нестабильных сетях.
 const (
     // IdleTimeout – время бездействия, после которого соединение принудительно закрывается
-    IdleTimeout = 5 * time.Minute
+    IdleTimeout = 10 * time.Minute // было 5 минут
 
     // DialTimeout – максимальное время на установку TCP-соединения
-    DialTimeout = 10 * time.Second
+    DialTimeout = 15 * time.Second // было 10 секунд
 
     // ReadTimeout – таймаут на чтение данных из соединения
-    ReadTimeout = 30 * time.Second
+    ReadTimeout = 60 * time.Second // было 30 секунд
 
     // WriteTimeout – таймаут на запись данных в соединение
-    WriteTimeout = 30 * time.Second
+    WriteTimeout = 60 * time.Second // было 30 секунд
 
     // HTTPTimeout – общий таймаут для HTTP-запросов (не CONNECT)
-    HTTPTimeout = 30 * time.Second
+    HTTPTimeout = 60 * time.Second // было 30 секунд
 )
