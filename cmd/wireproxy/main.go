89e54198fc23ac9569e3f75537c2b7f418931044
ageich <53635625+ageich@users.noreply.github.com>
2026-07-21 09:26:45 +0700

    Update main.go

--- cmd/wireproxy/main.go
+++ cmd/wireproxy/main.go
@@ -34,7 +34,7 @@ var defaultConfigPaths = []string{
 }
 
 // version – переопределяется при сборке через -ldflags
-var version = "1.0.22-dev"
+var version = "1.0.23-test"
 
 func lock(stage string) error {
 	switch stage {
